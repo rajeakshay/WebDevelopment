a2bedcbd3cccc62ec7d0f641c20f9ff3759368a4
Akshay Raje <raje.akshay@gmail.com>
2016-05-12 10:03:23 -0400

    Changed connection string for MongoDB on Openshift

--- test/app.js
+++ test/app.js
@@ -14,10 +14,11 @@ module.exports = function(app)
     //         process.env.OPENSHIFT_APP_NAME;
     // }
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PORT) {
-        connectionString = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-            process.env.OPENSHIFT_APP_NAME;
+    // Openshift environment variable MONGODB_URL is set by the custom cartridge for
+    // latest version of MongoDB (3.2.6).
+    // See: https://github.com/rajeakshay/openshift-cartridge-mongodb/tree/mongo3.2.6
+    if(process.env.MONGODB_URL) {
+        connectionString =  process.env.MONGODB_URL + process.env.OPENSHIFT_APP_NAME;
     }
 
     var mongoose = require("mongoose");
