cdcebec450136ffff22d5e86fd14bb716e421b1e
Akshay Raje <raje.akshay@gmail.com>
2016-05-12 09:44:37 -0400

    Moving to custom cartridge with MongoDB 3.2.6

--- test/app.js
+++ test/app.js
@@ -6,10 +6,16 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-            process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-            process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    // if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    //     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    //         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    //         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    //         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    //         process.env.OPENSHIFT_APP_NAME;
+    // }
+
+    if(process.env.OPENSHIFT_MONGODB_DB_PORT) {
+        connectionString = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
             process.env.OPENSHIFT_APP_NAME;
     }
