ea5c3a03d9ffc3430dd5373cdcf041883fde064a
Akshay Raje <raje.akshay@gmail.com>
2016-05-31 02:27:07 -0400

    Added binding and routing for website pages

--- public/assignment/views/page/page-edit.controller.client.js
+++ public/assignment/views/page/page-edit.controller.client.js
@@ -1,3 +1,40 @@
-/**
- * Created by amraje on 5/30/16.
- */
+(function(){
+	angular
+		.module("WebAppMaker")
+		.controller("EditPageController", EditPageController);
+
+	function EditPageController($routeParams, $location, PageService){
+		var vm = this;
+		vm.userId = $routeParams.uid;
+		vm.websiteId = $routeParams.wid;
+		vm.pageId = $routeParams.pid;
+
+		function init() {
+			vm.page = angular.copy(PageService.findPageById(vm.pageId));
+		}
+		init();
+
+		vm.updatePage = function updatePage(){
+			if (!vm.page || !vm.page.name || !vm.page.title) {
+				vm.error = "Check the page name and title.";
+			}
+			else {
+				var flag = PageService.updatePage(vm.pageId, vm.page);
+				if (flag) {
+					$location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+				} else {
+					vm.error = "Failed to update page.";
+				}
+			}
+		};
+
+		vm.deletePage = function deletePage() {
+			var flag = PageService.deletePage(vm.pageId);
+			if(flag) {
+				$location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+			} else {
+				vm.error = "Failed to delete page.";
+			}
+		}
+	}
+})();
--- public/assignment/views/page/page-new.controller.client.js
+++ public/assignment/views/page/page-new.controller.client.js
@@ -1,3 +1,25 @@
-/**
- * Created by amraje on 5/30/16.
- */
+(function(){
+	angular
+		.module("WebAppMaker")
+		.controller("NewPageController", NewPageController);
+
+	function NewPageController($routeParams, $location, PageService){
+		var vm = this;
+		vm.userId = $routeParams.uid;
+		vm.websiteId = $routeParams.wid;
+		vm.createPage = createPage;
+
+		function createPage() {
+			if (!vm.page || !vm.page.name || !vm.page.title) {
+				vm.error = "Check the page name or title.";
+			}else{
+				var result = PageService.createPage(vm.websiteId, vm.page);
+				if (result) {
+					$location.url("/user/" + vm.userId + "/website/" + vm.websiteId + "/page");
+				} else {
+					vm.error = "Failed to create page.";
+				}
+			}
+		}
+	}
+})();
