729f0a63fb3c2c6d272d5ee01db0dec0564d8c45
Akshay Raje <raje.akshay@gmail.com>
2016-06-29 19:19:06 -0400

    Users can now only add Youtube videos which are not in their favorites

--- public/project/views/user/discover.controller.client.js
+++ public/project/views/user/discover.controller.client.js
@@ -20,8 +20,29 @@
 			YoutubeService
 				.search(keywords, vm.youtubeKey)
 				.then(
-					function(response){
-						compileResults(response.data);
+					function(ytResponse){
+						ProjectUserService
+							.getFavoritesForUser(vm.user._id)
+							.then(
+								function(userFavorites){
+									var mine = compilePseudoFavorites(userFavorites.data);
+									var pseudoResults = compilePseudoResults(ytResponse.data);
+									if(pseudoResults.length != 0 && mine.length != 0){
+										for(var i = 0; i < pseudoResults.length; i ++){
+											for(var j = 0; j < mine.length; j++){
+												if(pseudoResults[i].id == mine[j].videoId){
+													pseudoResults[i].added = true;
+													break;
+												}
+											}
+										}
+									}
+									vm.videoResults = pseudoResults;
+								},
+								function(error){
+									compileResults(ytResponse.data);
+								}
+							);
 					},
 					function(err){
 						vm.videoResults = [];
@@ -39,6 +60,39 @@
 				s += c;
 			}
 			return s;
+		}
+
+		function compilePseudoFavorites(data){
+			var f = [];
+			for (var i = data.length - 1; i >= 0; i--) {
+				f.push({
+					videoId: data[i].videoId
+				});
+			}
+			return f;
+		}
+
+		function compilePseudoResults(data){
+			var f = [];
+			for (var i = data.items.length - 1; i >= 0; i--) {
+				f.push({
+					id: data.items[i].id.videoId,
+					url: $sce.trustAsResourceUrl("https://www.youtube.com/embed/" + data.items[i].id.videoId),
+					title: padding_right(data.items[i].snippet.title.replace(/(\r\n|\n|\r)/gm,""), " ", 25),
+					description: padding_right(data.items[i].snippet.description.replace(/(\r\n|\n|\r)/gm,""), " ", 60),
+					thumbnail: data.items[i].snippet.thumbnails.default.url,
+					author: padding_right(data.items[i].snippet.channelTitle, " ", 50),
+					original: {
+						videoId: data.items[i].id.videoId,
+						title: data.items[i].snippet.title,
+						author: data.items[i].snippet.channelTitle,
+						description: data.items[i].snippet.description
+					},
+					added: false,
+					error: false
+				});
+			}
+			return f;
 		}
 
 		function compileResults(data){
